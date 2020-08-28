46fcf59d7bf3603ec95772f061051cdcec40b4c3
jc_99 <jack.clayton@my.jcu.edu.au>
2020-08-28 10:32:12 +1000

    test commit features

--- Practicals/Prac_02/capitalist_conrad.py
+++ Practicals/Prac_02/capitalist_conrad.py
@@ -6,6 +6,7 @@ a 50% chance it increases by 0 to 10%, and
 a 50% chance that it decreases by 0 to 5%.
 If the price rises above $1000, or falls below $0.01, the program should end.
 The price should be displayed to the nearest cent (e.g. $33.59, not $33.5918232901)
+gggggggg
 """
 import random
 
